869f5c48f59d9bb3eaeff581102e7680b5f1c7e6
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-02-18 15:24:29 -0300

    aula de variaveis

---
+++ javascript - incial/exer.js
@@ -0,0 +1,49 @@
+       //EXERCICIOS variaveis
+
+
+//Declara uma variavel com a sua idade
+const number = 18
+
+//Declarar uma variavel com a sua comida preferida e atribuir um valor
+let hambuguer 
+hambuguer = "Melhor comida do mundo"
+
+// Declarar uma variavel e nao dar valor pra ela, sendo undefined
+var endereco
+
+// Declarar 5 variaveis diferentes sem valores
+var pessoa, rua, carro, moradia,CPF
+
+// Declarar uma variavel contendo uma string
+let casa = "Sobrado"
+
+// Declare uma varivel contendo um numero dentro de uma string
+let numerodacasa = "157"
+
+// Declare uma variavel com seu nome, e outra com sobrenome, e concatene os dois, depois faça um template string dos 2
+var nome,sobrenome
+console.log("Meu nome é" + nome + "e meu "+ sobrenome)
+console.log(`Meu nome é ${nome} ${sobrenome}`)
+nome = "Jean"
+sobrenome = "Romoaldo"
+
+
+// Coloque a seguinte frase em uma variavel: It´s time
+var naosei = "It´s time"
+
+console.log(typeof number, typeof hambuguer, typeof endereco, typeof pessoa, typeof rua,typeof moradia, typeof CPF, typeof casa, typeof numerodacasa, typeof nome, typeof sobrenome, typeof naosei )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// verifique o tipo de todas as variaveis criadas
